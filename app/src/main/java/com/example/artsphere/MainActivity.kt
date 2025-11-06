067a9fd5d155f050d36167dd3474e9608607aaea
Alyssa N <anajera@bu.edu>
2025-11-06 17:21:33 -0500

    fixed login UI

--- app/src/main/java/com/example/artsphere/MainActivity.kt
+++ app/src/main/java/com/example/artsphere/MainActivity.kt
@@ -1,13 +1,10 @@
 package com.example.artsphere
 
-import ads_mobile_sdk.h5
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
@@ -15,35 +12,22 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material.icons.filled.AccountCircle
-import androidx.compose.material.icons.filled.CheckCircle
-import androidx.compose.material.icons.filled.DateRange
-import androidx.compose.material.icons.filled.Edit
 import androidx.compose.material.icons.filled.Email
 import androidx.compose.material.icons.filled.Home
 import androidx.compose.material.icons.filled.LocationOn
 import androidx.compose.material.icons.filled.Lock
-import androidx.compose.material.icons.filled.MailOutline
-import androidx.compose.material.icons.filled.Star
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -51,24 +35,19 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.PasswordVisualTransformation
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import androidx.navigation.NavDestination.Companion.hierarchy
 import androidx.navigation.NavGraph.Companion.findStartDestination
-import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
-import androidx.navigation.navArgument
 import com.example.artsphere.ui.theme.ArtSphereTheme
 
 class MainActivity : ComponentActivity() {
@@ -171,7 +150,7 @@ fun MainScreen() {
             modifier = Modifier.padding(innerPadding)
         ) {
             composable(Screen.Login.route) { LoginScreen(navController) }
-            composable(Screen.Home.route) { GenericScreen(Screen.Home) }
+            composable(Screen.Home.route) { HomeScreen() }
             composable(Screen.Map.route) { GenericScreen(Screen.Map) }
             composable(Screen.Inbox.route) { GenericScreen(Screen.Inbox) }
             composable(Screen.Profile.route) { GenericScreen(Screen.Profile) }
@@ -280,10 +259,55 @@ fun LoginScreen(navController: NavController) {
     }
 }
 
-@Preview(showBackground = true)
 @Composable
-fun MainScreenPreview() {
+fun HomeScreen(){
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        Text("hi")
+    }
+}
+
+
+
+@Preview(showBackground = true, name = "Login Screen")
+@Composable
+fun LoginScreenPreview() {
     ArtSphereTheme {
-        MainScreen()
+        val navController = rememberNavController()
+        LoginScreen(navController)
+    }
+}
+
+@Preview(showBackground = true, name = "Home Screen")
+@Composable
+fun HomeScreenPreview() {
+    ArtSphereTheme {
+        HomeScreen()
+    }
+}
+
+@Preview(showBackground = true, name = "Map Screen")
+@Composable
+fun MapScreenPreview() {
+    ArtSphereTheme {
+        GenericScreen(Screen.Map)
+    }
+}
+
+@Preview(showBackground = true, name = "Inbox Screen")
+@Composable
+fun InboxScreenPreview() {
+    ArtSphereTheme {
+        GenericScreen(Screen.Inbox)
+    }
+}
+
+@Preview(showBackground = true, name = "Profile Screen")
+@Composable
+fun ProfileScreenPreview() {
+    ArtSphereTheme {
+        GenericScreen(Screen.Profile)
     }
 }
