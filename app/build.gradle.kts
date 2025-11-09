1e8f7e1f8b50b0fb6c4ae3bb52778a0322d0150a
lindaccy <lindaccy@bu.edu>
2025-11-09 15:32:41 -0500

    edit gradle and json file

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,7 +2,9 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+    id("com.google.gms.google-services")
 }
+
 
 android {
     namespace = "com.example.artsphere"
@@ -40,7 +42,6 @@ android {
 }
 
 dependencies {
-
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
@@ -49,6 +50,16 @@ dependencies {
     implementation(libs.androidx.compose.ui.graphics)
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
+
+    // Firebase BOM
+    implementation(platform("com.google.firebase:firebase-bom:33.4.0"))
+
+    // You need this for login
+    implementation("com.google.firebase:firebase-auth")
+
+    // You can keep analytics if you want
+    implementation("com.google.firebase:firebase-analytics")
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
