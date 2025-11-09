990e62009cdfcb57dc576e4fd4d0ec0a035be9f5
lindaccy <lindaccy@bu.edu>
2025-11-09 16:36:46 -0500

    testing git

--- app/src/main/java/com/example/artsphere/MainScreenWithBottomBar.kt
+++ app/src/main/java/com/example/artsphere/MainScreenWithBottomBar.kt
@@ -17,6 +17,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 
+//home page after we login
 @Composable
 fun MainScreenWithBottomBar() {
     var selectedTab by remember { mutableStateOf(BottomTab.Home) }
