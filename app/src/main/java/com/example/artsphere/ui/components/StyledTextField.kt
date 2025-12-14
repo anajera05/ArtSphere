8885120b38cbcfc488b33c36cc6c79d1f30bbc74
Alyssa N <anajera@bu.edu>
2025-12-14 18:10:33 -0500

    documentation for StyledTextField component

--- app/src/main/java/com/example/artsphere/ui/components/StyledTextField.kt
+++ app/src/main/java/com/example/artsphere/ui/components/StyledTextField.kt
@@ -22,6 +22,23 @@ import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.unit.dp
 
+/**
+ * A reusable component for displaying OutlinedTextField ensuring consistent styling
+ *
+ * @param value value being manipulated by the user
+ * @param onValueChange Callback triggered when the value changes.
+ * @param label label of the field
+ * @param icon icon to the left of the field, default null
+ * @param keyboardType specified keyboard type, default Text
+ * @param isPassword checks if value being inputted is a password, default false
+ * @param passwordVisible checks if password is visible, default false
+ * @param onPasswordVisibilityChange Callback triggered when password visibility changes.
+ * @param isSubmitted checks if the field has been submitted, default false
+ * @param lines number of lines the field can take, default 1
+ * @param modifier Modifier for the field, default fillMaxWidth()
+ * @param supportingText text to be displayed below the field, default null
+ *
+ */
 @Composable
 fun StyledTextField(
     value: String,
@@ -41,6 +58,7 @@ fun StyledTextField(
         value = value,
         onValueChange = onValueChange,
         label = { Text(text = label) },
+        // if icon is not null, display it
         leadingIcon = if (icon != null) {
             {
                 Icon(
@@ -52,6 +70,7 @@ fun StyledTextField(
         } else {
             null
         },
+        // if password, display visibility icon
         trailingIcon = {
             if (isPassword) {
                 IconButton(onClick = onPasswordVisibilityChange) {
@@ -63,6 +82,7 @@ fun StyledTextField(
                 }
             }
         },
+        // if password, hide text
         visualTransformation = if (isPassword && !passwordVisible) PasswordVisualTransformation() else VisualTransformation.None,
         modifier = modifier,
         maxLines = lines,
@@ -70,6 +90,7 @@ fun StyledTextField(
         shape = RoundedCornerShape(16.dp),
         colors = OutlinedTextFieldDefaults.colors(
             focusedBorderColor = MaterialTheme.colorScheme.primary,
+            // if submitted and empty, show error
             unfocusedBorderColor = if (isSubmitted && value.isBlank()) Color.Red else Color.Gray,
             focusedLabelColor = MaterialTheme.colorScheme.primary,
             unfocusedLabelColor = MaterialTheme.colorScheme.onSecondary,
@@ -80,7 +101,9 @@ fun StyledTextField(
         keyboardOptions = KeyboardOptions(
             keyboardType = keyboardType
         ),
+        // if submitted and empty, show error
         isError = isSubmitted && value.isBlank(),
+        // if supportingText is not null, display it
         supportingText = if (supportingText != null) {
             {
                 Text(
